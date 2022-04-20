c73abbccf9712e77e944c15439d9ffe2b018bceb
Tea <icepie.dev@gmail.com>
2022-04-20 17:44:15 +0800

    fix error

--- ESPBattery.cpp
+++ ESPBattery.cpp
@@ -9,26 +9,31 @@
 
 /////////////////////////////////////////////////////////////////
 
-ESPBattery::ESPBattery(byte analog_pin, int polling_interval_ms, int min_lvl, int max_lvl) {
+ESPBattery::ESPBattery(byte analog_pin, int polling_interval_ms, int min_lvl, int max_lvl)
+{
   pin = analog_pin;
   interval = polling_interval_ms;
-  min_level = min_lvl;  
+  min_level = min_lvl;
   max_level = max_lvl;
-  
+
   readData();
 }
 
 /////////////////////////////////////////////////////////////////
 
-void ESPBattery::readData() {
+void ESPBattery::readData()
+{
   last_read = millis();
   level = analogRead(pin);
-  v = (float) level / 1000 / (10000. / (47000 + 10000));
+  v = (float)level / 1000 / (10000. / (47000 + 10000));
   percentage = map(level, min_level, max_level, 0, 100);
 
-  if (percentage < 0) {
-    percentage = 0;  
-  } else if (percentage > 100) {
+  if (percentage < 0)
+  {
+    percentage = 0;
+  }
+  else if (percentage > 100)
+  {
     percentage = 100;
   }
   getState();
@@ -36,108 +41,138 @@ void ESPBattery::readData() {
 
 /////////////////////////////////////////////////////////////////
 
-void ESPBattery::loop() {
-  if (millis() > last_read + interval) {
+void ESPBattery::loop()
+{
+  if (int(millis()) > last_read + interval)
+  {
     readData();
   }
 }
 
 /////////////////////////////////////////////////////////////////
 
-float ESPBattery::getVoltage() {
+float ESPBattery::getVoltage()
+{
   return v;
 }
 
 /////////////////////////////////////////////////////////////////
 
-int ESPBattery::getPercentage() {
+int ESPBattery::getPercentage()
+{
   return percentage;
 }
 
 /////////////////////////////////////////////////////////////////
 
-int ESPBattery::getLevel() {
+int ESPBattery::getLevel()
+{
   return level;
 }
 
 /////////////////////////////////////////////////////////////////
 
-int ESPBattery::getState() {
-    last_state = state;
-    if (level >= ESPBATTERY_CHARGING) {
-        state = ESPBATTERY_CHARGING;
-    } else if (level >= ESPBATTERY_FULL) {
-        state = ESPBATTERY_FULL;        
-    } else if (level <= ESPBATTERY_CRITICAL) {
-        state = ESPBATTERY_CRITICAL;        
-    } else if (level <= ESPBATTERY_LOW) {
-        state = ESPBATTERY_LOW;        
-    } else {
-        state = ESPBATTERY_OK;
+int ESPBattery::getState()
+{
+  last_state = state;
+  if (level >= ESPBATTERY_CHARGING)
+  {
+    state = ESPBATTERY_CHARGING;
+  }
+  else if (level >= ESPBATTERY_FULL)
+  {
+    state = ESPBATTERY_FULL;
+  }
+  else if (level <= ESPBATTERY_CRITICAL)
+  {
+    state = ESPBATTERY_CRITICAL;
+  }
+  else if (level <= ESPBATTERY_LOW)
+  {
+    state = ESPBATTERY_LOW;
+  }
+  else
+  {
+    state = ESPBATTERY_OK;
+  }
+
+  if (state != last_state)
+  {
+    if (changed_cb != NULL)
+      changed_cb(*this);
+    switch (state)
+    {
+    case ESPBATTERY_CHARGING:
+      if (charging_cb != NULL)
+        charging_cb(*this);
+      break;
+    case ESPBATTERY_CRITICAL:
+      if (critical_cb != NULL)
+        critical_cb(*this);
+      break;
+    case ESPBATTERY_LOW:
+      if (low_cb != NULL)
+        low_cb(*this);
+      break;
     }
-        
-    if (state != last_state) {
-        if (changed_cb != NULL) changed_cb (*this);    
-        switch (state) {
-            case ESPBATTERY_CHARGING:
-                if (charging_cb != NULL) charging_cb (*this);    
-                break;
-            case ESPBATTERY_CRITICAL:
-                if (critical_cb != NULL) critical_cb (*this);    
-                break;
-            case ESPBATTERY_LOW:
-                if (low_cb != NULL) low_cb (*this);    
-                break;
-        }   
-    }
-    return state;
+  }
+  return state;
 }
 
 /////////////////////////////////////////////////////////////////
 
-String ESPBattery::stateToString(int state) {
-	switch (state) {
-		case ESPBATTERY_OK:
-			return "OK";
-		case ESPBATTERY_FULL:
-			return "FULL";
-		case ESPBATTERY_CHARGING:
-			return "CHARGING";
-		case ESPBATTERY_CRITICAL:
-			return "CRITICAL";
-		case ESPBATTERY_LOW:
-			return "LOW";
-	}
+String ESPBattery::stateToString(int state)
+{
+  switch (state)
+  {
+  case ESPBATTERY_OK:
+    return "OK";
+  case ESPBATTERY_FULL:
+    return "FULL";
+  case ESPBATTERY_CHARGING:
+    return "CHARGING";
+  case ESPBATTERY_CRITICAL:
+    return "CRITICAL";
+  case ESPBATTERY_LOW:
+    return "LOW";
+  default:
+    return "UNKNOWN";
+  }
 }
 
 /////////////////////////////////////////////////////////////////
 
-int ESPBattery::getPreviousState() {
-    return last_state;
+int ESPBattery::getPreviousState()
+{
+  return last_state;
 }
 
 /////////////////////////////////////////////////////////////////
 
-void ESPBattery::setLevelChangedHandler(CallbackFunction f) {
-    changed_cb = f;
+void ESPBattery::setLevelChangedHandler(CallbackFunction f)
+{
+  changed_cb = f;
 }
 
 /////////////////////////////////////////////////////////////////
 
-void ESPBattery::setLevelLowHandler(CallbackFunction f) {
-    low_cb = f;
+void ESPBattery::setLevelLowHandler(CallbackFunction f)
+{
+  low_cb = f;
 }
 
 /////////////////////////////////////////////////////////////////
 
-void ESPBattery::setLevelCriticalHandler(CallbackFunction f) {
-    critical_cb = f;
+void ESPBattery::setLevelCriticalHandler(CallbackFunction f)
+{
+  critical_cb = f;
 }
 
 /////////////////////////////////////////////////////////////////
 
-void ESPBattery::setLevelChargingHandler(CallbackFunction f) {
-    charging_cb = f;    
+void ESPBattery::setLevelChargingHandler(CallbackFunction f)
+{
+  charging_cb = f;
 }
 
 /////////////////////////////////////////////////////////////////
